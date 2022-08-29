24902f67921416acb5e4a43067374210ba1288f9
= <=>
2022-08-29 11:20:39 +0300

    production

--- src/resources/user/user.router.ts
+++ src/resources/user/user.router.ts
@@ -5,12 +5,8 @@ import {
   fetchAllUsers,
   fetchUserByEmail,
   updateUser,
-<<<<<<< HEAD
-  fetchUserById
-=======
   fetchUserById,
   topContributors
->>>>>>> 581d1ce6ced5109a94b7dfc0824e2c5cfa3b3986
 } from './user.controllers'
 import { respond } from '../../middlewares/respond'
 import { verifyToken } from '../../middlewares/verifyToken'
@@ -23,8 +19,5 @@ userRouter.post('/deleteAll', deleteAll, respond)
 userRouter.delete('/:email', deleteUser, respond)
 userRouter.put('/:id', verifyToken, updateUser, respond)
 userRouter.get('/', verifyToken, fetchUserById, respond)
-<<<<<<< HEAD
-=======
 userRouter.get('/topContributors', topContributors)
->>>>>>> 581d1ce6ced5109a94b7dfc0824e2c5cfa3b3986
 export = userRouter
