ef798866a1936af63bd131d8fb629f2fa0d528a5
Brook Zewdu <brookzewdu09@gmail.com>
2022-09-07 18:04:27 +0300

    dsa

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -2,7 +2,7 @@ import JWT from 'jsonwebtoken'
 export const generateToken = (user) => {
   return JWT.sign(
     {
-      id: user._id
+      _id: user._id
     },
     process.env.ACCESS_TOKEN_SECRET
   )
