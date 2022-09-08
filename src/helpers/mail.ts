c80fd2ec38d3cae0f43ca0393ce7c35f5d76fdaf
Brook Zewdu <brookzewdu09@gmail.com>
2022-09-08 23:42:54 +0300

    mail fix

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -18,18 +18,18 @@ export const sendMail = async (credentials: any) => {
         html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
             <div style="margin:50px auto;width:70%;padding:20px 0">
                 <div style="border-bottom:1px solid #eee">
-                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Digital Library</a>
+                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">ATRONS</a>
                 </div>
                 <p style="font-size:1.1em">Hi, ${credentials.to}</p>
                 <h4>Please verify your email</h4>
-                <p>Thank you for choosing Digital Library Platform. Use the following Code to complete your Sign Up procedures.</p>
+                <p>Thank you for choosing the ATRONS Platform. Use the following Code to complete your Sign Up procedures.</p>
                 <h4 style="background: #00466a;margin: 0 auto;width: max-content;padding: 10px 30px;color: #fff;border-radius: 4px;">
                 ${credentials.OTP}
                 </h4>
-                <p style="font-size:0.9em;">Regards,<br />Digital Library</p>
+                <p style="font-size:0.9em;">Regards,<br />ATRONS</p>
                 <hr style="border:none;border-top:1px solid #eee" />
                 <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-                <p>Digital Library Inc</p>
+                <p>ATRONS Inc</p>
                 <p>Addis Ababa</p>
                 <p>Ethiopia</p>
                 </div>
@@ -46,19 +46,19 @@ export const sendMail = async (credentials: any) => {
         html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
             <div style="margin:50px auto;width:70%;padding:20px 0">
                 <div style="border-bottom:1px solid #eee">
-                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Digital Library</a>
+                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">ATRONS</a>
                 </div>
                 <p style="font-size:1.1em">Hi, ${credentials.to}</p>
                 <h4>Please verify your email</h4>
-                <p>Thank you for choosing Digital Library Platform. Use the following Code to complete your Sign Up procedures.</p>
+                <p>Thank you for choosing ATRONS Platform. Use the following Code to complete your Sign Up procedures.</p>
                 <p>Click the link below change your password</p>
                 <a href = "${credentials.link}" style="margin: 0 auto;width: max-content;padding: 10px 30px;color: blue; text-decoration: none;">
                 ${credentials.link}
                 </a>
-                <p style="font-size:0.9em;">Regards,<br />Digital Library</p>
+                <p style="font-size:0.9em;">Regards,<br />ATRONS</p>
                 <hr style="border:none;border-top:1px solid #eee" />
                 <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-                <p>Digital Library Inc</p>
+                <p>ATRONS Inc</p>
                 <p>Addis Ababa</p>
                 <p>Ethiopia</p>
                 </div>
